525ff19e0f15188f77364dbc0bd8545194b44c99
Anshul Kapoor <anshulkapoor018@gmail.com>
2017-11-20 19:57:55 +0530

    Timestamp added

---
+++ playground/time.js
@@ -0,0 +1,7 @@
+// Unix epoch Jan 1st 1970 00:00:00 utc
+var moment =require('moment');
+
+var date = moment();
+console.log("\n");
+console.log('\t',date.format('MMM Do YYYY, h:mm a'));
+console.log("\n");
