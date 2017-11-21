0a68051eee8bc8551e7af0711f8a950a8e35ac2e
Anshul Kapoor <anshulkapoor018@gmail.com>
2017-11-21 13:40:54 +0530

    Storage Classes

---
+++ server/utils/users.test.js
@@ -0,0 +1,78 @@
+const expect = require('expect');
+
+const {Users} = require('./users');
+
+describe('Users', () => {
+  var users;
+
+  beforeEach(() => {
+    users = new Users();
+    users.users = [{
+      id: '1',
+      name: 'Anshul',
+      room: 'Django'
+    }, {
+      id: '2',
+      name: 'Akshay',
+      room: 'Django'
+    }, {
+      id: '3',
+      name: 'Rohan',
+      room: 'None'
+    }];
+  });
+
+  it('should add new user', () => {
+    var users = new Users();
+    var user = {
+      id: '123',
+      name: 'Anshul',
+      room: 'Django'
+    };
+    var resUser = users.addUser(user.id, user.name, user.room);
+
+    expect(users.users).toEqual([user]);
+  });
+
+  it('should remove a user', () => {
+    var userId = '1';
+    var user = users.removeUser(userId);
+
+    expect(user.id).toBe(userId);
+    expect(users.users.length).toBe(2);
+  });
+
+  it('should not remove user', () => {
+    var userId = '99';
+    var user = users.removeUser(userId);
+
+    expect(user).toNotExist();
+    expect(users.users.length).toBe(3);
+  });
+
+  it('should find user', () => {
+    var userId = '2';
+    var user = users.getUser(userId);
+
+    expect(user.id).toBe(userId);
+  });
+
+  it('should not find user', () => {
+    var userId = '99';
+    var user = users.getUser(userId);
+
+    expect(user).toNotExist();
+  });
+
+  it('should return names for Django', () => {
+    var userList = users.getUserList('Django');
+
+    expect(userList).toEqual(['Anshul', 'Akshay']);
+  });
+
+  it('should return names for None', () => {
+    var userList = users.getUserList('None');
+
+    expect(userList).toEqual(['Rohan']);
+  });
+});
